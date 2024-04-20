8672ad91629adf03abd076bcf8c44b5302c04bfe
aymanhaque <muhd.ayman.haque@gmail.com>
2024-04-20 00:24:43 -0500

    Fixed UI for mobile

--- loginPage/src/App.jsx
+++ loginPage/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div className="text-white">
-      <div className=" text-white  h-screen flex flex-col w-1/3 backdrop-blur-lg border-r-4" >
+      <div className=" text-white  h-screen flex flex-col sm:w-dvw lg:w-1/3 backdrop-blur-lg border-r-4" >
       <Logo>
       </Logo>
       <div className=" h-screen flex justify-center items-center">
--- loginPage/src/pages/signupPage.jsx
+++ loginPage/src/pages/signupPage.jsx
@@ -5,7 +5,7 @@ import '../index.css'
 function SignupPage() {
   return (
     <div className="text-white">
-      <div className="h-screen flex flex-col w-1/3 backdrop-blur-lg border-r-4" flex>
+      <div className="text-white  h-screen flex flex-col sm:w-dvw lg:w-1/3 backdrop-blur-lg border-r-4" flex>
       <Logo>
       </Logo>
       <div className=" h-screen flex justify-center items-center">

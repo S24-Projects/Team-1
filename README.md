a73f6f3bf728cbaaec57a872b908bc47bb956a8e
aymanhaque <muhd.ayman.haque@gmail.com>
2024-09-27 16:04:00 -0500

    Fixed GoogleButton

--- README.md
+++
@@ -1,2 +0,0 @@
-# Team-1
-Team Members: Aryan Neeli, Sanyukta Bhadlikar, Muhd Haque
--- loginPage/src/components/GoogleComponent.jsx
+++ loginPage/src/components/GoogleComponent.jsx
@@ -1,34 +1,47 @@
 import { GoogleLogin } from "@react-oauth/google";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-const clientID = "262889882503-oacf0knn91ga7u4lmnoau4g0aege32ek.apps.googleusercontent.com"
-
-
+const clientID =
+  "262889882503-oacf0knn91ga7u4lmnoau4g0aege32ek.apps.googleusercontent.com";
 
 function GoogleButton() {
-    const usenavigate = useNavigate();
-    
-    const onSuccess = (res) => {
-        console.log("Login Success", res);
-        usenavigate('./TestHome');
-    }
+  const usenavigate = useNavigate();
 
-    const onFailure = (res) => {
-        console.log("Login Failed", res);
-    }
-    return(
-    <div id = "singInButton"> 
+//   const onSuccess = (res) => {
+//     console.log("Login Success", res);
+//     usenavigate("./TestHome");
+//   };
+
+//   const onFailure = (res) => {
+//     console.log("Login Failed", res);
+//   };
+  return (
+    <div id="singInButton">
+      <GoogleOAuthProvider clientId={clientID}>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse);
+            usenavigate("./TestHome");
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
         
-        <GoogleOAuthProvider><GoogleLogin
+      </GoogleOAuthProvider>
+    </div>
+  );
+}
+
+export default GoogleButton;
+
+{
+  /* <GoogleLogin
             clientId = {clientID}
             buttonText = "Login"
             onSuccess = {onSuccess}
             onFailure = {onFailure}
             cookiePolicy = {'single-host-origin'}
             isSignedIn = {true}
-        /></GoogleOAuthProvider>
-    </div>
-    )
+        /> */
 }
-
-export default GoogleButton
